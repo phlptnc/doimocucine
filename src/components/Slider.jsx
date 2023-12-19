cb46b8d5849731658d328510374ecc9a153b35d4
Philip Joseph <139831746+phlptnc@users.noreply.github.com>
2023-12-19 14:24:01 +0800

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,4 @@
 import React from "react"
-import RoundLink from "./Button/RoundLink"
-import Slider1 from "../assets/home/slides-1.webp"
-import FadeInScroll from "./Animations/FadeInScroll"
 import Banner from "./Banner"
 
 const slidesData = [
@@ -55,27 +52,6 @@ function Slider() {
                         style="before:bg-white justify-center uppercase"
                     />
                 </div>
-                {/* <div className=" bg-[pink] w-full h-full relative outline outline-1 outline-yellow-400 py-40">
-                    <div className=" w-full h-full relative">
-                        <div className="wrapper h-full w-full absolute left-0 top-0 flex flex-row">
-                            {slidesData.map((item, index) => (
-                                <div
-                                    key={index}
-                                    className="w-[80%] h-full px-[4vw] absolute top-0 left-0"
-                                >
-                                    <div className="">
-                                        <img
-                                            src={item.src}
-                                            alt=""
-                                            className=" h-full w-full"
-                                        />
-                                    </div>
-                                    <p>{item.description}</p>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div> */}
             </section>
         </>
     )
